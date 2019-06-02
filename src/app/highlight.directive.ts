3ceb770dcbd5099bbef4be9b7aaf860b8527e82b
JamesMutahi <mutahijames0@gmail.com>
2019-06-02 23:18:48 +0300

    Attempt to add custom directive

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+import { Quote } from './quote';
+
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+   @Input() quote: Quote;
+
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.backgroundColor = 'transparent';
+  }
+
+    @HostListener('click') onClicks() {
+      this.highlight('red');
+    }
+
+    private highlight(action: string){
+
+      this.elem.nativeElement.style.backgroundColor = action;
+    }
+
+
+
+
+}
