706e9a50dbe95c607596a433484313ae1952c259
JamesMutahi <mutahijames0@gmail.com>
2019-06-02 17:28:17 +0300

    Create array to hold quotes and create cards to display quotes

--- src/app/quote.spec.ts
+++ src/app/quote.spec.ts
@@ -1,7 +1,35 @@
-import { Quote } from './quote';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe('Quote', () => {
-  it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Quotes'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Quotes');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Quotes!');
   });
 });
