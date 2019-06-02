8de3187ee0fcdfcfc5013ee3af869d8d5cd0a98c
JamesMutahi <mutahijames0@gmail.com>
2019-06-02 23:16:13 +0300

    Add additioanl styling and functionality to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
     QuoteComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    HighlightDirective,
+    TimeAgoPipe,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
