aa23bd9b04d0d80670473845f82e5ee46d9ec5e2
JamesMutahi <mutahijames0@gmail.com>
2019-06-04 12:36:55 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-import { DatePipe } from '@angular/common';
-export class Quote {
-  length: number;
-  constructor(public id: number, public author: string, public quote: string, public thief: string, public date: Date) {
-  }
-}
